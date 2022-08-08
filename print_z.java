1398a63ab01dba63bc5cd4006af13bcbde168c9b
Ashish-Singh-7 <110215670+Ashish-Singh-7@users.noreply.github.com>
2022-08-08 14:57:00 +0530

    Update print_z.java

--- print_z.java
+++ print_z.java
@@ -5,8 +5,8 @@ public class print_z{
 public static void main(String[] args) {
 
 System.out.println("*****");
-System.out.println(" *");
-System.out.println(" *");
+System.out.println("   *");
+System.out.println("  *");
 System.out.println(" *");
 System.out.println("*****");
 }
