93acf92a6d64dcd5b2483a1f8d3b95ab1c98146c
Ashish-Singh-7 <110215670+Ashish-Singh-7@users.noreply.github.com>
2022-08-13 11:55:07 +0530

    Add files via upload

---
+++ Pattern22.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Pattern22{
+    public static void main(String[] args) {
+
+        System.out.println("    *****");
+        System.out.println("   *****");
+        System.out.println("  *****");
+        System.out.println(" *****");
+        System.out.println("*****");
+    }
+    
+}
