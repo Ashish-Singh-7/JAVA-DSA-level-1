1f7b1edff44090b8cce5dcd090deb27262f2e28e
Ashish-Singh-7 <110215670+Ashish-Singh-7@users.noreply.github.com>
2022-08-15 19:23:22 +0530

    Add files via upload

---
+++ getting_started3.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class getting_started3 {
+    public static void main(String[] args) {
+        
+        Scanner scn=new Scanner(System.in);
+        int n=scn.nextInt();
+
+        int count=0;
+
+        for(int i=1;i<=n;i++)
+        {
+            count+=i;
+            System.out.println(count);
+        }
+    }
+}
