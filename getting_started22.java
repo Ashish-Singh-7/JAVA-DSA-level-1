fa5df56b5bf7cd15ee7c6d540e5f8bfbd6e9eaa4
Ashish-Singh-7 <110215670+Ashish-Singh-7@users.noreply.github.com>
2022-08-24 19:51:46 +0530

    Add files via upload

---
+++ getting_started22.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class getting_started22 {
+    public static void main(String[] args) {
+        System.out.println("*****\n");
+        System.out.println("   *\n");
+        System.out.println("  *\n");
+        System.out.println(" *\n" );
+        System.out.println("*****\n");
+    }
+}
