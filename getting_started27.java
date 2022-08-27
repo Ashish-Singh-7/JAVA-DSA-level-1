6c0df114f7b2903ada1ef7d8d37b989d236bb959
Ashish-Singh-7 <110215670+Ashish-Singh-7@users.noreply.github.com>
2022-08-27 18:58:13 +0530

    Add files via upload

---
+++ getting_started27.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class getting_started27 {
+    public static void main(String[] args) {
+        Scanner scn=new Scanner(System.in);
+        int n=scn.nextInt();
+
+        int no_of_dig=0;
+        int temp=n;
+        while(temp!=0)
+        {
+            temp=temp/10;
+            no_of_dig++;
+        }
+
+        int div=(int)Math.pow(10,no_of_dig-1);
+        while(div!=0)
+        {
+            int q=n/div;
+            System.out.println(q);
+
+            n=n%div;
+            div=div/10;
+        }
+    }
+}
