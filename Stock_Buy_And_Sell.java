020ad17993ca4c83e1214d235b0d27a687b15784
Ashish-Singh-7 <110215670+Ashish-Singh-7@users.noreply.github.com>
2023-04-20 18:08:39 +0530

    Add files via upload

---
+++ Stock_Buy_And_Sell.java
@@ -0,0 +1,67 @@
+import java.util.*;
+public class Stock_Buy_And_Sell{
+    public static void main(String[] args){
+        Scanner scn=new Scanner(System.in);
+        int n=scn.nextInt();
+        int loss=0;
+        float percentage=0.0;
+        int profit=0;
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=scn.nextInt();
+            percentage=sp;
+            if(Stoc)
+        }
+        int max=arr[0];
+        int min=arr[0];
+        for(int i=1;i<n;i++)
+        {
+            if(arr[i]>max)
+            {
+                max=arr[i];
+                System.out.println(i);
+            }
+            else if(arr[i]<min)
+            {
+                min=arr[i];
+                int ma'
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                >System.out.println(i);
+
+            }
+            profit=max-min;
+            loss=min-max;
+            System.out.println(profit);
+            while(<max{})
+        for(int i=0;i<n;i++)
+        {
+            if(arr[ax)
+            {
+                max=arr[i];
+            }
+            else if(arr[i]<min)
+            {
+                min=arr[i];
+            }
+            System.out.println(loss(min-max));
+        }
+    }
+}
