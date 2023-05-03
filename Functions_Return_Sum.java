5c68115b18ea4610c794e60d1a81123b1b214078
Ashish-Singh-7 <110215670+Ashish-Singh-7@users.noreply.github.com>
2023-05-03 21:25:54 +0530

    Add files via upload

---
+++ Functions_Return_Sum.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Functions_Return_Sum{
+    public static int CalculateSum(int a,int b){
+        int sum=a+b;
+        return sum;
+    }
+    public static void main(String[] args){
+        Scanner scn=new Scanner(System.in);
+        int a=scn.nextInt();
+        int b=scn.nextInt();
+        int sum=CalculateSum(a,b);
+        System.out.println(sum);
+    }
+}
