c89a2505f5abfdb617e1b203b3948aa86abeceb6
Ashish-Singh-7 <110215670+Ashish-Singh-7@users.noreply.github.com>
2023-05-12 21:33:35 +0530

    Add files via upload

---
+++ Bit.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class Bit{
+    public static void main(String[] args){
+        Scanner scn=new Scanner(System.in);
+        int n=scn.nextInt();
+        int X=0;
+        while(n-->=1)
+        {
+            String str=scn.next();
+            if(str.charAt(0)=='X')
+            {
+                if(str.charAt(1)=='+'){
+                    X++;
+                }
+                if(str.charAt(1)=='-'){
+                    X--;
+                }
+            }
+            else if(str.charAt(0)=='+')
+            {
+                ++X;
+            }
+            else
+            {
+                --X;
+            }
+        } 
+        System.out.println(X);
+    }
+}
