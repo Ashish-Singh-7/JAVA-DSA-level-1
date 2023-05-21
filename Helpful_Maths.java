4bf96b9a59c3d12a1c4ce14416e1461c4b40d54e
Ashish-Singh-7 <110215670+Ashish-Singh-7@users.noreply.github.com>
2023-05-21 17:55:38 +0530

    Add files via upload

---
+++ Helpful_Maths.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Helpful_Maths{
+    public static void main(String[] args){
+        Scanner scn=new Scanner(System.in);
+        String s=scn.nextLine();
+        ArrayList<Integer> al=new ArrayList<>();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)!='+'){
+                int n=s.charAt(i)-48;
+                al.add(n);
+            }
+        }
+        Collections.sort(al);
+        for(int i=0;i<al.size();i++)
+        {
+            if(i!=al.size()-1)
+                System.out.print(al.get(i)+"+");
+            else System.out.print(al.get(i));
+        }
+    }
+}
