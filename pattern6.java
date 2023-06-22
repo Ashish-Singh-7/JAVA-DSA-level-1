d8d94cb380270f53f9cfa2450396ad1a94d9a146
Ashish-Singh-7 <110215670+Ashish-Singh-7@users.noreply.github.com>
2023-06-22 12:58:50 +0530

    Add files via upload

--- pattern6.java
+++ pattern6.java
@@ -1,33 +1,12 @@
-import java.util.Scanner;
-
-public class pattern6 {
+import java.util.*;
+public class pattern6{
     public static void main(String[] args){
-        Scanner scn=new Scanner(System.in);
-        int n=scn.nextInt();
-
-        int star = n / 2 + 1;
-        int space = 1;
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= star; j++) {
-                System.out.print("*\t");
+        for(int i=1;i<=5;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print(j+" ");
             }
-
-            for (int j = 1; j <= space; j++) {
-                System.out.print("\t");
-            }
-
-            for (int j = 1; j <= star; j++) {
-                System.out.print("*\t");
-            }
-
-            if (i <= n / 2) {
-                space += 2;
-                star--;
-            } else {
-                space -= 2;
-                star++;
-            }
-
             System.out.println();
         }
     }
