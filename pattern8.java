3340e545d0d2076a4d055eac9fb6dfe755d824c6
Ashish-Singh-7 <110215670+Ashish-Singh-7@users.noreply.github.com>
2023-06-22 13:24:01 +0530

    Add files via upload

--- pattern8.java
+++ pattern8.java
@@ -1,26 +1,15 @@
-import java.util.Scanner;
-
-public class pattern8 {
+import java.util.*;
+public class pattern8{
     public static void main(String[] args){
-        Scanner scn=new Scanner(System.in);
-        int n=scn.nextInt();
-
-        for(int i=1;i<=n;i++)
+        int number=1;
+        for(int i=1;i<=5;i++)
         {
-            for(int j=1;j<=n;j++)
+            for(int j=1;j<=i;j++)
             {
-                if(i+j==n+1)
-                {
-                    System.out.print("*\t");
-                }
-                else
-                {
-                    System.out.print("\t");
-                }
+                System.out.print(number+" ");
+                number+=1;
             }
             System.out.println();
         }
-
-        
     }
 }
