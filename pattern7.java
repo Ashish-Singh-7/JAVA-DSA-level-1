895b2c37227e30ec2a0ed44f60d8bb66083f415d
Ashish-Singh-7 <110215670+Ashish-Singh-7@users.noreply.github.com>
2023-06-22 13:12:46 +0530

    Add files via upload

--- pattern7.java
+++ pattern7.java
@@ -1,22 +1,11 @@
-import java.util.Scanner;
-
-public class pattern7 {
+import java.util.*;
+public class pattern7{
     public static void main(String[] args){
-        Scanner scn=new Scanner(System.in);
-        int n=scn.nextInt();
-        
-        for(int i=1;i<=n;i++)
+        for(int i=1;i<=5;i++)
         {
-            for(int j=1;j<=n;j++)
+            for(int j=1;j<=5-i+1;j++)
             {
-                if(i==j)
-                {
-                    System.out.print("*\t");
-                }
-                else
-                {
-                    System.out.print("\t");
-                }
+                System.out.print(j);
             }
             System.out.println();
         }
