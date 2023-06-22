c6a8385e3a7468a9eee77e435ec9971552f3a768
Ashish-Singh-7 <110215670+Ashish-Singh-7@users.noreply.github.com>
2023-06-22 11:47:43 +0530

    Add files via upload

--- pattern2.java
+++ pattern2.java
@@ -1,15 +1,18 @@
-import java.util.Scanner;
-
-public class pattern2 {
-    public static void main(String[] args) {
-        Scanner scn=new Scanner(System.in);
-        int n=scn.nextInt();
-
-        for(int i=n;i>=1;i--)
+import java.util.*;
+public class pattern2{
+    public static void main(String[] args){
+        for(int i=1;i<=4;i++)
         {
-            for(int j=1;j<=i;j++)
+            for(int j=1;j<=5;j++)
             {
-                System.out.print("*\t");
+                if(j>=2 && j<=4 && i>=2 && i<=3)
+                {
+                    System.out.print(" ");
+                }
+                else
+                {
+                    System.out.print("*");
+                }
             }
             System.out.println();
         }
