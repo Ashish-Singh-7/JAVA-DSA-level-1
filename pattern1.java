02a8e1d7c6183803b831d8a5f4709492f05bdf52
Ashish-Singh-7 <110215670+Ashish-Singh-7@users.noreply.github.com>
2023-06-22 11:34:21 +0530

    Add files via upload

--- pattern1.java
+++ pattern1.java
@@ -1,15 +1,11 @@
-import java.util.Scanner;
-
+import java.util.*;
 public class pattern1{
     public static void main(String[] args){
-        Scanner scn=new Scanner(System.in);
-        int n=scn.nextInt();
-
-        for(int i=1;i<=n;i++)
+        for(int i=1;i<=4;i++)
         {
-            for(int j=1;j<=i;j++)
+            for(int j=1;j<=5;j++)
             {
-                System.out.print("*\t");
+                System.out.print("*");
             }
             System.out.println();
         }
