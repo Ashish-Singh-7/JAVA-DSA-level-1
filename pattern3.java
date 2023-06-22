026095e4c9cbe605f64526b0f0711fa65a37ed90
Ashish-Singh-7 <110215670+Ashish-Singh-7@users.noreply.github.com>
2023-06-22 12:04:51 +0530

    Add files via upload

--- pattern3.java
+++ pattern3.java
@@ -1,28 +1,13 @@
-import java.util.Scanner;
-
-public class pattern3 {
+import java.util.*;
+public class pattern3{
     public static void main(String[] args){
-        Scanner scn=new Scanner(System.in);
-        int n=scn.nextInt();
-
-        int space=n-1;
-        int star=1;
-        for(int i=1;i<=n;i++)
+        for(int i=1;i<=4;i++)
         {
-            for(int j=1;j<=space;j++)
+            for(int j=1;j<=i;j++)
             {
-                System.out.print("\t");
+                System.out.print("*");
             }
-            for(int j=1;j<=star;j++)
-            {
-                System.out.print("*\t");
-            }
-            
-            space--;
-            star++;
             System.out.println();
         }
     }
 }
-
-        
