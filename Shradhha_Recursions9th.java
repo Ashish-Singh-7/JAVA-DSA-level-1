62d904f9702f63c6ec8db7aefc04b2f99e3ff9a5
Ashish-Singh-7 <110215670+Ashish-Singh-7@users.noreply.github.com>
2023-07-10 22:30:13 +0530

    Add files via upload

---
+++ Shradhha_Recursions9th.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class Shradhha_Recursions9th{
+    public static int first=-1;
+    public static int last=-1;
+    public static void findOccurence(String str,int index,char element){
+        if(index==str.length()){
+            System.out.println(first);
+            System.out.println(last);
+            return;
+        }
+        char currChar=str.charAt(index);
+        if(currChar==element)
+        {
+            if(first==-1)
+            {
+                first=index;
+            }
+            else
+            {
+                last=index;
+            }
+        }
+        findOccurence(str,index+1,element);
+    }
+    public static void main(String[] args){
+        String str="abaabcdaacf";
+        findOccurence(str,0,'a');
+    }
+}
